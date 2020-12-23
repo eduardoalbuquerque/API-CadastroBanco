e580a6b8c07a8379de97f83243bff2f4b0fb6417
Eduardo Albuquerque <eduardo.albuquerque@hotmail.com.br>
2020-12-23 20:59:00 -0300

    started our project with basic structure

---
+++ src/main/java/com/orangetalents/apicadastro/OrangetalentszupApplication.java
@@ -0,0 +1,13 @@
+package com.orangetalents.apicadastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrangetalentszupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrangetalentszupApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/orangetalents/apicadastro/OrangetalentszupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orangetalents.apicadastro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrangetalentszupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
