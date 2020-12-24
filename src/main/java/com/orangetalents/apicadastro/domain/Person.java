1e045d4c22be41172d8e4a11ed07d3f82f2b3e82
Eduardo Albuquerque <eduardo.albuquerque@hotmail.com.br>
2020-12-24 11:06:34 -0300

    using Hibernate for persistence Person class in to the Postgres

--- src/main/java/com/orangetalents/apicadastro/domain/Person.java
+++ src/main/java/com/orangetalents/apicadastro/domain/Person.java
@@ -2,13 +2,20 @@ package com.orangetalents.apicadastro.domain;
 
 import lombok.Data;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 @Data
+@Entity
 public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false, length = 100)
     private String name;
+    @Column(nullable = false, length = 60)
     private String email;
+    @Column(nullable = false, length = 14)
     private String cpf;
     private LocalDate birthday;
 }
